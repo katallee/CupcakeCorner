30ef5485ccadd29623980caf79e6a1bf924925f9
Kat Allee <kallee@butlercc.edu>
2021-04-21 10:22:49 -0500

    start of cupcake corner project

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -4,11 +4,57 @@
 //
 //  Created by Kat Allee on 4/21/21.
 //
+// TO DO
+//CHECK FOR A VALID ADDRESS
+//PREPARE FOR CHECKOUT
+//ENCODING AN OBSERVABLEOBJECT CLASS
+//SENDING AND RECEIVING ORDERS OVER THE INTERNET
+//WRAP UP
+
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var order = Order()
+    
     var body: some View {
-        Text("Hello")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+                    
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+                        
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery details")
+                    }
+                }
+            }
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 class Order: ObservableObject {
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
@@ -13,7 +14,14 @@ class Order: ObservableObject {
     @Published var type = 0
     @Published var quantity = 3
     
-    @Published var specialRequestEnabled = false
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
     @Published var extraFrosting = false
     @Published var addSprinkles = false
 }
